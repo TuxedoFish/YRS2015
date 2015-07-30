5654d750e2c9362337e6025f92b2710ea681e5a7
TuxedoFish <harryliversedge@gmail.com>
2015-07-30 20:22:11 +0100

    fixed sql adding

--- php/db_request.php
+++ php/db_request.php
@@ -9,7 +9,7 @@ $dailymail = $_GET['dailymail'];
 $times = $_GET['times'];
 $sun = $_GET['sun'];
 
-if ($user_id = 'new') {
+if ($user_id == 'new') {
     query_db("INSERT INTO tblUsers(bbc, guardian, telegraph, dailymail, times, sun) VALUES ($bbc, $guardian, $telegraph, $dailymail, $times, $sun)");
     echo mysql_insert_id();
 }
@@ -20,6 +20,7 @@ query_db("UPDATE tblUsers SET telegraph = telegraph + " . $telegraph . " WHERE u
 query_db("UPDATE tblUsers SET dailymail = dailymail + " . $dailymail . " WHERE user_id='" . $user_id . "'");
 query_db("UPDATE tblUsers SET times = times + " . $times . " WHERE user_id='" . $user_id . "'");
 query_db("UPDATE tblUsers SET sun = sun + " . $sun . " WHERE user_id='" . $user_id . "'"); 
+query_db("UPDATE tblUsers SET total = total + " . "1" . " WHERE user_id='" . $user_id . "'"); 
 }
 
 
--- php/mysql_login.php
+++ php/mysql_login.php
@@ -4,18 +4,18 @@ $db_database = 'dbWhereDoYouFall';
 $db_username = 'root';
 $db_password = '';
 
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+$db_server = @mysql_connect($db_hostname, $db_username, $db_password);
 if (!$db_server) {
-    die("Unable to connect to MySQL: " . mysql_error());
+    die("Unable to connect to MySQL: " . @mysql_error());
 }
 
-mysql_select_db($db_database)
-	or die("Unable to select database: " . mysql_error());
+@mysql_select_db($db_database)
+	or die("Unable to select database: " . @mysql_error());
 
 function query_db($query) {
-    $result = mysql_query($query);
+    $result = @mysql_query($query);
     if (!$result) {
-    die("Database access failed: " . mysql_error()); }
+    die("Database access failed: " . @mysql_error()); }
     return $result;
 }
 ?>
