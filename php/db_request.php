2df62fe44e040edc1e0f823d9c9c875973630436
MattGJLewis <donotuse@donotuse.invalid>
2015-07-30 11:45:36 +0100

    db_request.php now returns ID of a new user

--- php/db_request.php
+++ php/db_request.php
@@ -9,15 +9,18 @@ $dailymail = $_GET['dailymail'];
 $times = $_GET['times'];
 $sun = $_GET['sun'];
 
-if ($user_id = '') {
+if ($user_id = 'new') {
     query_db("INSERT INTO tblUsers(bbc, guardian, telegraph, dailymail, times, sun) VALUES ($bbc, $guardian, $telegraph, $dailymail, $times, $sun)");
+    echo mysql_insert_id();
 }
-
+else {
 query_db("UPDATE tblUsers SET bbc = bbc + " . $bbc . " WHERE user_id='" . $user_id . "'");
 query_db("UPDATE tblUsers SET guardian = guardian + " . $guardian . " WHERE user_id='" . $user_id . "'");
 query_db("UPDATE tblUsers SET telegraph = telegraph + " . $telegraph . " WHERE user_id='" . $user_id . "'");
 query_db("UPDATE tblUsers SET dailymail = dailymail + " . $dailymail . " WHERE user_id='" . $user_id . "'");
 query_db("UPDATE tblUsers SET times = times + " . $times . " WHERE user_id='" . $user_id . "'");
-query_db("UPDATE tblUsers SET sun = sun + " . $sun . " WHERE user_id='" . $user_id . "'");
+query_db("UPDATE tblUsers SET sun = sun + " . $sun . " WHERE user_id='" . $user_id . "'"); 
+}
+
 
 ?>
